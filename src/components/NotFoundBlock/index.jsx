77f34b2c20e455a42f2ec0236aed261f671da6f1
AVK <avkovaley@gmail.com>
2023-11-01 18:21:17 +0300

    +@media

--- src/components/NotFoundBlock/index.jsx
+++ src/components/NotFoundBlock/index.jsx
@@ -9,7 +9,7 @@ function NotFoundBlock() {
     return (
         <div className={styles.root}>
             <h1 style={{ color: "#fe5f1e", fontSize: '100px', marginBottom: '50px' }}> Not Found :( </h1 >
-            <img src={ImgNotFound} alt="Oh, no!" />
+            <img className={styles.img__nf} src={ImgNotFound} alt="Oh, no!" />
         </div>
     )
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,8 @@ import EmptyCart from '../assets/img/empty-cart.png'
 
 
 
+
+
 function Cart() {
     return (
         <div>
